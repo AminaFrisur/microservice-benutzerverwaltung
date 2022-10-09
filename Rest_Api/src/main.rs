f26bfad8b172ec1d9c0f4c522b77c87f9b52b777
Tim Hoeffner <tim.florian.hoeffner@gmail.com>
2022-10-09 16:34:24 +0200

    created getUsers, register and changeUser methods for bentzerverwaltung restapis. Tim.

--- Rest_Api/src/main.rs
+++ Rest_Api/src/main.rs
@@ -5,100 +5,104 @@
 use postgres::{Client, NoTls};
 use rocket_contrib::json::Json;
 use serde::{Serialize, Deserialize};
+use magic_crypt::{new_magic_crypt, MagicCryptTrait};
 
+// TODO: GENEREAL:
 // TODO: Implement Error Handling
 // TODO: Authentifizierung innerhalb der MS Architektur noch festlegen
 // TODO: Rust nightly -> Wirklich empfehlenswert fuer production? -> gibt es auch eine Alternative um rocket zu kompelieren ?
 
-// NOTE: i16 deshalb verwendet stat u16, da SQL Trait nicht mit u16 arbeitet
+// TODO: Benutzerverwaltung:
+// TODO: Passwort speichern: -> Dort mal bitte schauen ob MagicCrypt ausreichend ist ! -> Generell die Grundlagen zur Passwort verschlüsselung anschauen !
+// TODO: Passwort Reset implementieren -> Dort mal bitte schauen ob MagicCrypt ausreichend ist ! -> Generell die Grundlagen zur Passwort verschlüsselung anschauen !
+// TODO: rocket toml Datie anlegen um port auch zu wechseln
+// TODO: Change reveiving port for postgres db
+// TODO: Create Login function -> erstelle ein Auth Token dann -> dafür noch eine neue bib suchen !
 
-fn create_fahrzeug_list(query_result: Vec<postgres::row::Row>) -> Vec<Fahrzeug> {
 
-    let mut vec_fahrzeuge : Vec<Fahrzeug> = Vec::new();
+fn create_users_list(query_result: Vec<postgres::row::Row>) -> Vec<User> {
+
+    let mut vec_user : Vec<User> = Vec::new();
 
     for row in query_result {
 
-        let fahrzeug = Fahrzeug {
+        let user = User {
             id: row.get(0),
-            marke: row.get(1),
-            model: row.get(2),
-            leistung: row.get(3),
-            plz: row.get(4),
+            email: row.get(1),
+            firstname: row.get(2),
+            lastname: row.get(3),
+            street: row.get(4),
+            house_number: row.get(5),
+            postal_code: row.get(6),
+            login_name: row.get(7)
         };
-        vec_fahrzeuge.push(fahrzeug);
+        vec_user.push(user);
     }
-    return vec_fahrzeuge;
+    return vec_user;
 }
 
-
-
-#[get("/getVehicle/<plz>/<id>")]
-fn get_vehicle(id: i32, plz: i32) -> Json<Vec<Fahrzeug>> {
-
+#[get("/getUsers")]
+fn get_users() -> Json<Vec<User>> {
     let mut client = Client::connect("host=database port=5432 user=postgres password=test", NoTls).unwrap();
-    let query_result = client.query("SELECT id, marke, model, leistung, plz FROM fahrzeuge WHERE id=$1 AND plz=$2 AND active=TRUE",  &[&id, &plz]).unwrap();
-    return Json(create_fahrzeug_list(query_result));
-
+    let query_result = client.query("SELECT id, email, firstname, lastname, street, house_number, postal_code, login_name FROM users", &[]).unwrap();
+    return Json(create_users_list(query_result));
 }
 
-#[get("/getVehicles/<plz>")]
-fn get_vehicles(plz: i32) -> Json<Vec<Fahrzeug>> {
-
+#[post("/changeUser", format = "json", data = "<user_json>")]
+fn change_user(user_json: Json<User>) -> String {
+    let user: User = user_json.into_inner();
     let mut client = Client::connect("host=database port=5432 user=postgres password=test", NoTls).unwrap();
-    let query_result = client.query("SELECT id, marke, model, leistung, plz FROM fahrzeuge WHERE plz=$1 AND active=TRUE",  &[&plz]).unwrap();
-    return Json(create_fahrzeug_list(query_result));
-
+    client.execute("UPDATE user SET email = $1, firstname = $2, lastname = $3 , street = $4 , house_number = $5 , postal_code = $6  WHERE id = $7",
+                   &[&user.email, &user.firstname, &user.lastname, &user.street ,&user.house_number, &user.postal_code, &user.id]).unwrap();
+    return format!("Updated successfully user with id: {}", user.id);
 }
 
-#[post("/updateVehicle", format = "json", data = "<fahrzeug_json>")]
-fn update_vehicle(fahrzeug_json: Json<Fahrzeug>) -> String {
+#[post("/register", format = "json", data = "<register_json>")]
+fn register(register_json: Json<Register>) -> Json<Vec<User>> {
+    let register: Register = register_json.into_inner();
 
-    let fahrzeug: Fahrzeug = fahrzeug_json.into_inner();
-
-    let mut client = Client::connect("host=database port=5432 user=postgres password=test", NoTls).unwrap();
-
-    client.execute("UPDATE fahrzeuge SET marke = $1, model = $2, leistung = $3 , plz = $4 WHERE id = $5",
-                   &[&fahrzeug.marke, &fahrzeug.model, &fahrzeug.leistung, &fahrzeug.plz ,&fahrzeug.id]).unwrap();
-
-    return format!("Updated successfully vehicle with id: {}", fahrzeug.id);
-}
-
-#[post("/addVehicle", format = "json", data = "<fahrzeug_json>")]
-fn add_vehicle(fahrzeug_json: Json<Fahrzeug>) -> Json<Vec<Fahrzeug>> {
-
-    let fahrzeug: Fahrzeug = fahrzeug_json.into_inner();
+    // encrypt password
+    let mc = new_magic_crypt!("magickey", 256);
+    let encrypted_password = String::from(mc.encrypt_str_to_base64(register.password));
 
     let mut client = Client::connect("host=database port=5432 user=postgres password=test", NoTls).unwrap();
-
-    client.execute("INSERT INTO fahrzeuge(marke, model, leistung, plz, active) VALUES ($1, $2, $3, $4, TRUE)",
-                   &[&fahrzeug.marke, &fahrzeug.model, &fahrzeug.leistung, &fahrzeug.plz]).unwrap();
-
-    let query_result = client.query("SELECT id, marke, model, leistung, plz FROM fahrzeuge WHERE plz = $1 ORDER BY id DESC LIMIT 1",
-                                    &[&fahrzeug.plz]).unwrap();
-
-    return Json(create_fahrzeug_list(query_result));
-}
-
-#[post("/inactiveVehicle/<id>")]
-fn inactive_vehicle(id: i32) -> Json<Vec<Fahrzeug>> {
-
-    let mut client = Client::connect("host=database port=5432 user=postgres password=test", NoTls).unwrap();
-    let query_result = client.query("SELECT id, marke, model, leistung, plz FROM fahrzeuge WHERE id=$1",  &[&id]).unwrap();
-
-    client.execute("UPDATE fahrzeuge SET active = FALSE WHERE id = $1", &[&id]).unwrap();
-
-    return Json(create_fahrzeug_list(query_result));
+    client.execute("INSERT INTO users(email, firstname, lastname, street, house_number, postal_code, login_name, password) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+                   &[&register.email, &register.firstname, &register.lastname, &register.street, &register.house_number, &register.postal_code, &register.login_name, &encrypted_password]).unwrap();
+    let query_result = client.query("SELECT id, email, firstname, lastname, street, house_number, postal_code, login_name FROM users ORDER BY id DESC LIMIT 1",
+                                    &[]).unwrap();
+    return Json(create_users_list(query_result));
 }
 
 fn main()  {
-    rocket::ignite().mount("/", routes![get_vehicle, get_vehicles, update_vehicle, add_vehicle, inactive_vehicle]).launch();
+    rocket::ignite().mount("/", routes![get_users, register, change_user]).launch();
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Fahrzeug {
+struct User {
     id: i32,
-    marke: String,
-    model: String,
-    leistung: i32,
-    plz: i32
+    email: String,
+    firstname: String,
+    lastname: String,
+    street: String,
+    house_number: i32,
+    postal_code: i32,
+    login_name: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Register {
+    email: String,
+    firstname: String,
+    lastname: String,
+    street: String,
+    house_number: i32,
+    postal_code: i32,
+    login_name: String,
+    password: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Login {
+    login_name: String,
+    password: String
 }
