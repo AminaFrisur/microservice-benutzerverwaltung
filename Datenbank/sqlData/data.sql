a44af89a8926941862a4c7553daa3825dd11b4f6
Tim Hoeffner <tim.florian.hoeffner@gmail.com>
2022-10-09 15:10:28 +0200

    changed data.sql. Translate all attributes and tablename into english. Tim.

--- Datenbank/sqlData/data.sql
+++ Datenbank/sqlData/data.sql
@@ -1,13 +1,13 @@
-CREATE TABLE IF NOT EXISTS benutzer (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     email varchar(255) NOT NULL,
-    vorname varchar(255) NOT NULL,
-    nachname varchar(255) NOT NULL,
-    straße varchar(255) NOT NULL,
-    hausnummer int NOT NULL,
-    plz int NOT NULL,
-    passwort varchar(255));
+    firstname varchar(255) NOT NULL,
+    lastname varchar(255) NOT NULL,
+    street varchar(255) NOT NULL,
+    house_number int NOT NULL,
+    postal_code int NOT NULL,
+    password varchar(255));
 
-INSERT INTO benutzer(email, vorname, nachname, straße, hausnummer, plz, passwort) VALUES ('max.musterman@test.de','max','musterman', 'In der Straße', 10, 50667, 'test123'),
+INSERT INTO users(email, firstname, lastname, street, house_number, postal_code, password) VALUES ('max.musterman@test.de','max','musterman', 'In der Straße', 10, 50667, 'test123'),
                                                                                          ('lisa.musterfrau@test.de','lisa','musterfrau', 'An der Gasse', 24, 10115, 'test123'),
                                                                                          ('tim.hoeffner@test.de','tim','hoeffner', 'Am Hof', 13, 53783, 'test123');
