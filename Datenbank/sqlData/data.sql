f5104c4adc1fad4f07c2fdc45b060b42d1cc3769
Tim Hoeffner <tim.florian.hoeffner@gmail.com>
2022-10-09 16:33:27 +0200

    changed users table again. Added login_name attribute and made it to primary key value. Tim.

--- Datenbank/sqlData/data.sql
+++ Datenbank/sqlData/data.sql
@@ -1,13 +1,14 @@
 CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
+    id SERIAL,
     email varchar(255) NOT NULL,
     firstname varchar(255) NOT NULL,
     lastname varchar(255) NOT NULL,
     street varchar(255) NOT NULL,
     house_number int NOT NULL,
     postal_code int NOT NULL,
+    login_name varchar(255) PRIMARY KEY,
     password varchar(255));
 
-INSERT INTO users(email, firstname, lastname, street, house_number, postal_code, password) VALUES ('max.musterman@test.de','max','musterman', 'In der Straße', 10, 50667, 'test123'),
-                                                                                         ('lisa.musterfrau@test.de','lisa','musterfrau', 'An der Gasse', 24, 10115, 'test123'),
-                                                                                         ('tim.hoeffner@test.de','tim','hoeffner', 'Am Hof', 13, 53783, 'test123');
+INSERT INTO users(email, firstname, lastname, street, house_number, postal_code, login_name, password) VALUES ('max.musterman@test.de','max','musterman', 'In der Straße', 10, 50667, 'testuser1', 'test123'),
+                                                                                                              ('lisa.musterfrau@test.de','lisa','musterfrau', 'An der Gasse', 24, 10115, 'testuser2', 'test123'),
+                                                                                                              ('tim.hoeffner@test.de','tim','hoeffner', 'Am Hof', 13, 53783, 'testuser3','test123');
