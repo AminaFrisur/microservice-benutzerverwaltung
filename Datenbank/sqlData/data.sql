c2df2947e3d6b936c57a0685d3a570387ddf1529
Tim Hoeffner <tim.florian.hoeffner@gmail.com>
2022-10-09 15:02:31 +0200

    created new table benutzer for benutzerverwaltung.

--- Datenbank/sqlData/data.sql
+++ Datenbank/sqlData/data.sql
@@ -1,9 +1,13 @@
-CREATE TABLE IF NOT EXISTS fahrzeuge (
-  model varchar(255) NOT NULL,
-  id SERIAL PRIMARY KEY,
-  marke varchar(255) NOT NULL,
-  leistung int NOT NULL,
-  active boolean DEFAULT FALSE,
-  plz int NOT NULL);
+CREATE TABLE IF NOT EXISTS benutzer (
+    id SERIAL PRIMARY KEY,
+    email varchar(255) NOT NULL,
+    vorname varchar(255) NOT NULL,
+    nachname varchar(255) NOT NULL,
+    straße varchar(255) NOT NULL,
+    hausnummer int NOT NULL,
+    plz int NOT NULL,
+    passwort varchar(255));
 
-INSERT INTO fahrzeuge(model, marke, leistung, active, plz) VALUES ('m3','BMW',450, TRUE, 50667),('RS6','Audi',550, TRUE, 10115),('Model S','Tesla',550, TRUE, 60306);
+INSERT INTO benutzer(email, vorname, nachname, straße, hausnummer, plz, passwort) VALUES ('max.musterman@test.de','max','musterman', 'In der Straße', 10, 50667, 'test123'),
+                                                                                         ('lisa.musterfrau@test.de','lisa','musterfrau', 'An der Gasse', 24, 10115, 'test123'),
+                                                                                         ('tim.hoeffner@test.de','tim','hoeffner', 'Am Hof', 13, 53783, 'test123');
