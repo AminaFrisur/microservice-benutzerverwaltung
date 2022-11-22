bd6ee1da259917e962b1a05d0116cfeb28cd6e20
Tim Hoeffner <tim.florian.hoeffner@gmail.com>
2022-11-23 00:11:04 +0100

    fixed sql statements for auth.

--- rest_api/src/auth.js
+++ rest_api/src/auth.js
@@ -1,4 +1,3 @@
-const http = require("http");
 module.exports = function() {
 
     const Pool = require('pg').Pool
@@ -12,8 +11,8 @@ module.exports = function() {
 
     var module = {};
 
-    let sqlAdmin = 'SELECT * FROM users WHERE login_name = $1 AND auth_token = $2 AND  10000000 > (SELECT EXTRACT(EPOCH FROM ((SELECT CURRENT_TIMESTAMP::timestamp) - auth_token_timestamp::timestamp)))';
-    let sqlUser = 'SELECT * FROM users WHERE is_admin = TRUE AND login_name = $1 AND auth_token = $2 AND  10000000 > (SELECT EXTRACT(EPOCH FROM ((SELECT CURRENT_TIMESTAMP::timestamp) - auth_token_timestamp::timestamp)))';
+    let sqlAdmin = 'SELECT * FROM users WHERE is_admin = TRUE AND login_name = $1 AND auth_token = $2 AND  10000000 > (SELECT EXTRACT(EPOCH FROM ((SELECT CURRENT_TIMESTAMP::timestamp) - auth_token_timestamp::timestamp)))';
+    let sqlUser = 'SELECT * FROM users WHERE login_name = $1 AND auth_token = $2 AND  10000000 > (SELECT EXTRACT(EPOCH FROM ((SELECT CURRENT_TIMESTAMP::timestamp) - auth_token_timestamp::timestamp)))';
 
     let getAuthTokenTimeStamp = 'SELECT login_name, auth_token, auth_token_timestamp, is_admin FROM users WHERE login_name = $1 AND auth_token = $2 AND  10000000 > (SELECT EXTRACT(EPOCH FROM ((SELECT CURRENT_TIMESTAMP::timestamp) - auth_token_timestamp::timestamp)))';
 
@@ -48,7 +47,7 @@ module.exports = function() {
     }
     
     function unvalidTokenResponse(res) {
-        res.status(401).send("token and/or login name are missing or are not valid");
+        res.status(401).send("Token wurde nicht gefunden oder ist nicht mehr valide. Nutzer muss sich neues Token beziehen!");
     }
 
     module.checkAuthUser = function(req, res, next) {
@@ -72,8 +71,9 @@ module.exports = function() {
                     res.status(401).send(error);
                     return;
                 } 
-                
+
                 if(results.rows.length != 1) {
+                    console.log("RESULT IS 0")
                     unvalidTokenResponse(res);
                     return;
                 } else {
